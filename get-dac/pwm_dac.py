449b614b4d369b461c711296d54e99491d12fe48
RPi Ediz <karakocedizdogan@gmail.com>
2026-02-25 13:13:27 +0300

    4th lesson 4th commit

--- get-dac/pwm_dac.py
+++ get-dac/pwm_dac.py
@@ -1,36 +1,34 @@
 import RPi.GPIO as GPIO
 
 class PWM_DAC:
-    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
-        self.gpio_pin = gpio_pin
+    def __init__(self, gpio_bits, pwm_frequency, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
         self.pwm_frequency = pwm_frequency
         self.dynamic_range = dynamic_range
         self.verbose = verbose
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+
+        self.pwm = GPIO.PWM(self.gpio_bits, self.pwm_frequency)
+        self.pwm.start(0.0)
 
 
     def deinit(self):
-        GPIO.output(self.gpio_pin, 0)
+        GPIO.output(self.gpio_bits, 0)
         GPIO.cleanup()
 
 
-    def set_number(self, number):
-        if (0 <= number <= 255):
-            binary = [int(el) for el in bin(number)[2:].zfill(8)]
-            GPIO.output(self.gpio_pin, binary)
-        else:
-            GPIO.output(self.gpio_pin, 0)
-
-
-    def 
+    def set_voltage(self, voltage):
+        number = int(voltage / self.dynamic_range * 100)
+        self.pwm.ChangeDutyCycle(number)
 
 
 if __name__ == "__main__":
+    dac = PWM_DAC(12, 2000, 3.15, True)
     try:
-        dac = PWM_DAC(12, 500, 3.290, True)
-       
+
+
         while True:
             try:
                 voltage = float(input("Введите напряжение в Вольтах: "))
